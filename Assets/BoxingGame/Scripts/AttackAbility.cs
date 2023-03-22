8bc06d64d93d923f55fe76a2526ec4eb0eb4113e
Andrew <andrew@capelagames.com.au>
2023-03-22 17:13:56 +1100

    enemy turns

--- Assets/BoxingGame/Scripts/AttackAbility.cs
+++ Assets/BoxingGame/Scripts/AttackAbility.cs
@@ -9,7 +9,7 @@ public class AttackAbility : PlayerAbility
 		if(_turnTimer.IsNextTurn())
 		{
 			int damage = Random.Range(20, 30);
-			Debug.Log("ATTACK " + damage);
+			_enemy.DealDamage(damage);
 			EndTurn();
 		}
 	}
